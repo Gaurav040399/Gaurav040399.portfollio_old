7a058012874027f57ac0859d9c3b78c6596b3ddd
Gaurav Asode <gauravasode5@gmail.com>
2023-05-02 22:59:27 +0530

    contact section and footer

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-const reactCalender = require("react-github-calendar")
+// toggle icon navbar
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".category");
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle('bx-x');
+    category.classList.toggle('active')
+}
+
+// Scroll section active link
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("#nav-menu .category a");
+
+window.onscroll = ()=>{
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if(top >= offset&& top < offset+height){
+            navLinks.forEach(links =>{
+                links.classList.remove("active");
+                document.querySelector('#nav-menu .category a[href*=' + id + ']').classList.add('active')
+            });
+        }
+    });
+
+    // Sticky navbar
+    let header = document.querySelector("#nav-menu")
+
+    header.classList.toggle('sticky',window.scrollY > 100);
+}
